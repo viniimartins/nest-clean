e46f6aee4cb517fb96b55d9ffe04d92fbc7a319d
Vinicius Martins Ribeiro <vinimribeiro2004@gmail.com>
2025-09-15 23:15:09 -0300

    Controller de criação de conta

---
+++ src/controllers/create-account.controller.ts
@@ -0,0 +1,31 @@
+import { Body, ConflictException, Controller, HttpCode, Post } from "@nestjs/common";
+import type { PrismaService } from "src/prisma/prisma.service";
+
+@Controller('/account')
+export class CreateAccountController {
+  constructor(private readonly prisma: PrismaService) { }
+
+  @Post()
+  @HttpCode(201)
+  async handle(@Body() body: any) {
+    const { name, email, password } = body
+
+    const userWithSameEmail = await this.prisma.user.findUnique({
+      where: {
+        email,
+      },
+    })
+
+    if (userWithSameEmail) {
+      throw new ConflictException('User with same e-mail address already exists.')
+    }
+
+    const user = await this.prisma.user.create({
+      data: {
+        name,
+        email,
+        password,
+      },
+    })
+  }
+}
