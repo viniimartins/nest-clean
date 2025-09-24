899faa17557d56b69338d025e82ef1df6ef74a20
Vinicius Martins Ribeiro <vinimribeiro2004@gmail.com>
2025-09-23 21:45:16 -0300

    Gerando token JWT

--- src/env.ts
+++ src/env.ts
@@ -3,6 +3,8 @@ import { z } from 'zod'
 export const envSchema = z.object({
   DATABASE_URL: z.url(),
   JWT_SECRET: z.string(),
+  JWT_PRIVATE_KEY: z.string(),
+  JWT_PUBLIC_KEY: z.string(),
   PORT: z.coerce.number().optional().default(3333),
 })
 
